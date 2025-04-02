f251ee29c28f7a1b7f0b27fad8b1f4216e2a293d
asi160 <asi160@uclive.ac.nz>
2025-04-03 11:43:39 +1300

    did work

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+import os
+
 """
 main file to read pokerstars data.
 Please look at the readme for more infomation on how to use the software
 """
+
+def parse_individual_file(file_path):
+    """reads each individual file"""
+    with open(file_path, "r") as file:
+        content = file.read()
+        print(f"Contents of {file}:")
+        print(content)
+
+def parse_all_input_file():
+    """take the pokerstar data file and puts it in a readable format"""
+    for filename in os.listdir('pokerstar_raw_data_files'):
+        if filename.endswith(".txt"):
+            file_path = os.path.join("pokerstar_raw_data_files", filename)
+            parse_individual_file(file_path)
+
+def hand_counter():
+    """return the number of hands played from all files"""
+    pass
+
+def hand_counter_per_file():
+    """take an incomming file and return the number of hands played in each individual file"""
+    pass
+
+def main():
+    """main logic function"""
+    pass
